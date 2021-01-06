afb62ffac89a9ae166f7a1a9f6299db2e97dacb8
pankajkarwal92@gmail.com <pankajkarwal92@gmail.com>
2021-01-07 00:22:28 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import List from "./components/List";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <List />
+    </div>
+  );
+}
+
+export default App;
